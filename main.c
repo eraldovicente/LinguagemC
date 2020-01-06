aa3415f0843e2934c77985081915fe4ac3beb450
Eraldo Vicente <eraldovicente2@gmail.com>
2020-01-06 13:22:53 -0200

    Declaracao de variavel

--- main.c
+++ main.c
@@ -1,8 +1,24 @@
-#include <stdio.h> /*biblioteca de escrita e leitura*/
-#include <stdlib.h> /*biblioteca auxiliar*/
+#include <stdio.h> /**biblioteca de escrita e leitura*/
+#include <stdlib.h> /**biblioteca auxiliar*/
 
 int main()
 {
-    printf("Hello world!\n");
+    /**
+     Declaração de variáveis
+
+     tipo nome;
+     char
+     int
+     float
+     double
+    */
+
+    int nro = 10;
+    float n;
+    float n2;
+    float n3;
+    int nro1 = 10, nro2, nro3;
+
+    system("pause");
     return 0;
 }
