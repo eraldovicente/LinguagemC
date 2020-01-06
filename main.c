11e377e894fef341cda1eff633027424fd668ac5
Eraldo Vicente <eraldovicente2@gmail.com>
2020-01-06 13:39:37 -0200

    Comando printf

--- main.c
+++ main.c
@@ -3,21 +3,16 @@
 
 int main()
 {
-    /**
-     Declaração de variáveis
-
-     tipo nome;
-     char
-     int
-     float
-     double
-    */
-
+    char letra = 'a';
     int nro = 10;
-    float n;
-    float n2;
-    float n3;
-    int nro1 = 10, nro2, nro3;
+    float n = 5.25;
+    printf("%c \n", letra);
+    printf("%d \n", letra);
+    printf("%d \n", nro);
+    printf("%f \n", n);
+    printf("Meu programa C\n");
+    printf("Letra %c, inteiro %d \n", letra, nro);
+    printf("Nro: %d; Nro + 1: %d\n", nro, nro+1);
 
     system("pause");
     return 0;
